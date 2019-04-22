88dc7ca8a4c35cc952613ca016ff34956f9c0822
i81182 <binod.thapa@verscend.com>
2019-04-22 22:31:37 +0545

    lab10

---
+++ 25A/Lab10/Lab10/Calcutor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    public class Calcutor
+    {
+        public static int Add(int a,int b)
+        {
+            try
+            {
+                throw new Exception("It is manual error");
+                return a + b;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ 25A/Lab10/Lab10/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int m = Convert.ToInt32(textBox1.Text);
+                int n = Convert.ToInt32(textBox2.Text);
+                //Calcutor c = new Calcutor();
+                int sum = Calcutor.Add(m, n);
+                textBox3.Text = sum.ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
